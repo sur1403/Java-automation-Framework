cb77a55e5ca3c62a523210ebbf551e27738d0a87
sur1403 <surbhi.bansal1403@gmail.com>
2024-07-05 10:50:09 +1200

    pages

--- src/main/java/com/mystore/pageobjects/IndexPage.java
+++ src/main/java/com/mystore/pageobjects/IndexPage.java
@@ -1,4 +1,49 @@
 package com.mystore.pageobjects;
 
-public class IndexPage {
+import com.mystore.actiondriver.Action;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class IndexPage extends Action {
+    @FindBy(xpath = "//img[@alt = 'Shop Selenium Books']")
+    WebElement selBookImg;
+
+    @FindBy(xpath = "//img[@title = 'Selenium Ruby']")
+    WebElement selRubyImg;
+
+    @FindBy(xpath = "//img[@title = 'Thinking in HTML']")
+    WebElement thinkInHtmlImg;
+
+    @FindBy(xpath = "//img[@title = 'Mastering JavaScript']")
+    WebElement masterJSImg;
+
+    @FindBy(xpath = "//a[@href='/?add-to-cart=160']")
+    WebElement SelniumAddToCartBtn;
+
+    @FindBy(xpath = "//a[@href='/?add-to-cart=163']")
+    WebElement thinkInHTMLBtn;
+
+    @FindBy(xpath = "//a[@href='/?add-to-cart=165']")
+    WebElement masterJSBtn;
+
+    @FindBy(xpath = "//div[@id='n2-ss-6-arrow-previous']/img[@alt='Arrow']")
+    WebElement leftArrow;
+
+    @FindBy(xpath = "//div[@id='n2-ss-6-arrow-next']/img[@alt='Arrow']")
+    WebElement rightArrow;
+
+    public IndexPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickOnSeleniumImg(){
+        Action.click(driver, selRubyImg );
+    }
+
+    public void thinkInHTMLImg(){
+        Action.click(driver, thinkInHtmlImg );
+    }
+
+
 }
