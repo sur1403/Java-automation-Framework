1932953075b8c15f7005fa850f5595f8c02ab0c3
sur1403 <surbhi.bansal1403@gmail.com>
2024-07-08 20:23:59 +1200

    addition

--- src/test/java/com/mystore/testcases/LoginAccountTest.java
+++ src/test/java/com/mystore/testcases/LoginAccountTest.java
@@ -1,4 +1,22 @@
 package com.mystore.testcases;
 
-public class LoginAccountTest {
+import com.mystore.base.BaseClass;
+import com.mystore.pageobjects.IndexPage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class LoginAccountTest extends BaseClass {
+    IndexPage indexPage;
+
+    @BeforeMethod
+    public void setup() throws Throwable {
+        launchApp();
+        indexPage = new IndexPage();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
